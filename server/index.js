5f94aad24dbb9d5ce2863e8bd51365b1a84abeeb
liucanzhu <2250042762@qq.com>
2024-07-13 14:44:08 +0800

    use 1:scep crypto 2:toHex

--- server/index.js
+++ server/index.js
@@ -7,9 +7,9 @@ app.use(cors());
 app.use(express.json());
 
 const balances = {
-  "0x1": 100,
-  "0x2": 50,
-  "0x3": 75,
+  "03cefb09405fd79bc04de876242982e13d0d65fbecf4cbb975d8b8560831a4dffb": 100,
+  "03127eb345e669ebd43bdce5595f4a850d42d118c106084d64639db439e2448bf3": 50,
+  "0215a3741bed713118078d4a46a1f0b836e43253e2b00fa4b7dadc605a5e5f2dda": 75,
 };
 
 app.get("/balance/:address", (req, res) => {
@@ -23,7 +23,6 @@ app.post("/send", (req, res) => {
 
   setInitialBalance(sender);
   setInitialBalance(recipient);
-
   if (balances[sender] < amount) {
     res.status(400).send({ message: "Not enough funds!" });
   } else {
